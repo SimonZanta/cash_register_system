8c12f23dcc73930e6a02b1a721b99b27ae6b27b1
Šimon Žanta <simon.zanta@protonmail.com>
2024-04-29 15:04:36 +0200

    feat(frontend): finish regisrtation

--- frontend/reservation-system/app/registration/page.js
+++ frontend/reservation-system/app/registration/page.js
@@ -6,25 +6,32 @@ import { getCookie, setCookie } from "cookies-next";
 
 export default function page(){
     const router = useRouter()
-    const elementRef = useRef(); 
-    
-    const handleRegistration = () => {
-        router.push("/registration")
+    const elementRef = useRef();
+
+    const handleHome = () => {
+        router.push("/")
     }
+
 
     async function handleSubmit(e){
         e.preventDefault();
 
         const fname = elementRef.current.querySelector("#fname").value
+        const fsurname = elementRef.current.querySelector("#fsurname").value
+        const femail = elementRef.current.querySelector("#femail").value
+        const fusername = elementRef.current.querySelector("#fusername").value
         const fpasswd = elementRef.current.querySelector("#fpasswd").value
         
         const formData = {
-          username: fname,
-          password: fpasswd,
+            first_name: fname,
+            last_name: fsurname,
+            email: femail,
+            username: fusername,
+            password: fpasswd,
         };
     
         try {
-          const response = await fetch('http://localhost:8080/login', {
+          const response = await fetch('http://localhost:8080/register', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json'
@@ -45,12 +52,24 @@ export default function page(){
 
     return(
         <main>
-            <div className="mx-auto w-2/3 mt-24">
+            <div className="mx-auto w-2/3 mt-14">
             <form className="mx-auto w-80 border-2 border-mainText rounded-xl p-10" ref={elementRef} onSubmit={handleSubmit}>  
                 <div className="flex flex-col gap-5">
                     <div className="flex flex-col w-full">
                         <label className="text-base uppercase" htmlFor="fname">name</label>
                         <input required type="text" id="fname" name="fname" className="bg-mainBackground border-2 border-mainText rounded-md p-1"></input>
+                    </div>
+                    <div className="flex flex-col w-full">
+                        <label className="text-base uppercase" htmlFor="fsurname">surname</label>
+                        <input required type="text" id="fsurname" name="fsurname" className="bg-mainBackground border-2 border-mainText rounded-md p-1"></input>
+                    </div>
+                    <div className="flex flex-col w-full">
+                        <label className="text-base uppercase" htmlFor="femail">email</label>
+                        <input required type="text" id="femail" name="femail" className="bg-mainBackground border-2 border-mainText rounded-md p-1"></input>
+                    </div>
+                    <div className="flex flex-col w-full">
+                        <label className="text-base uppercase" htmlFor="fusername">username</label>
+                        <input required type="text" id="fusername" name="fusername" className="bg-mainBackground border-2 border-mainText rounded-md p-1"></input>
                     </div>
 
                     <div className="flex flex-col w-full">
@@ -59,7 +78,9 @@ export default function page(){
                     </div>
                 
                     <button className="w-full uppercase text-base border-2 border-mainText
-                    bg-secondaryBackground hover:bg-secondaryHover rounded-md p-1 mt-10" type="submit" id="loadBtn" >login</button>
+                    bg-secondaryBackground hover:bg-secondaryHover rounded-md p-1 mt-10" type="submit" id="loadBtn" >register</button>
+                    <button className="w-full uppercase text-base border-2 border-mainText
+                    bg-secondaryBackground hover:bg-secondaryHover rounded-md p-1" type="button" onClick={handleHome}>home</button>
                 </div>
             </form>
             
